d39636992fd74dc6b142b72be6fc4821d5725068
BielGonzalez <158280733+BielGonzalez@users.noreply.github.com>
2024-01-31 12:33:44 +0100

    Create bateria1.1.py

---
+++ bateria1.1.py
@@ -0,0 +1,5 @@
+#Programa que pregunta dos números a l'usuari i calcula la seva suma.
+num1 = int(input("Diguem un numero "))
+num2 = int(input("Diguem un altre numero "))
+resultat = int(float(num1) + float(num2))
+print("El resultat es: ",resultat)
