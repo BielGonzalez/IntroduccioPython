a19216b051b85c15a657c9461410a8210f8567f9
BielGonzalez <158280733+BielGonzalez@users.noreply.github.com>
2024-01-31 12:41:36 +0100

    Create bateria1.4.py

---
+++ bateria1.4.py
@@ -0,0 +1,4 @@
+#Programa que demana un pes en kg a l'usuari i retorna el mateix pes convertit en lliures.
+peskg = input("Quin es el teu pes en kg? ")
+peslib = int(peskg) * 2.20462
+print("El teu pes en lliures es: ",peslib)
