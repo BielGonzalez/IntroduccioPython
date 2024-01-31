858cd4ea2d697272a0b21c0ae78824ae9d7facf2
BielGonzalez <158280733+BielGonzalez@users.noreply.github.com>
2024-01-31 12:42:07 +0100

    Create bateria1.5.py

---
+++ bateria1.5.py
@@ -0,0 +1,4 @@
+#Programa que demana una temperatura en graus Celsius a l'usuari i retorna la temperatura en graus Fahrenheit.
+peskg = input("Quin es el teu pes en kg? ")
+peslib = int(peskg) * 2.20462
+print("El teu pes en lliures es: ",peslib)
