79e7d0631136a4e37ccb42c15ef25cd5632aa362
BielGonzalez <158280733+BielGonzalez@users.noreply.github.com>
2024-01-31 12:35:21 +0100

    Create bateria1.2.py

---
+++ bateria1.2.py
@@ -0,0 +1,4 @@
+#Programa que pregunta un número i retorna el seu quadrat.
+num1 = int(input("Diguem un numero "))
+resultat = float(num1) * float(num1)
+print("El numero que m'has dit al quadrat es: ",resultat)
