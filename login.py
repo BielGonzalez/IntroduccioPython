09c6d28d3f6d2ed4913a2ac473c873bc35ee5a17
BielGonzalez <158280733+BielGonzalez@users.noreply.github.com>
2024-02-01 23:00:08 +0100

    Create login.py

---
+++ login.py
@@ -0,0 +1,8 @@
+usuari = "Biel"
+contraseña = "smx"
+usuari_preguntat = str(input("Quin es el teu usuari? "))
+contraseña_preguntada = str(input("Quina es la teva contraseña? "))
+if usuari == usuari_preguntat and contraseña == contraseña_preguntada:
+	print("Inici de sessio correcte")
+else:
+	print("Hi ha hagut un error de autenticacio")
