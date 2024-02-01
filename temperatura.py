1c1dd938fd2c79611d116433383f8ae0d77528f3
BielGonzalez <158280733+BielGonzalez@users.noreply.github.com>
2024-02-01 22:59:46 +0100

    Create temperatura.py

---
+++ temperatura.py
@@ -0,0 +1,7 @@
+temperatura = int(input("Quina temperatura hi ha?"))
+if temperatura < 15:
+    print("Agafa una jaqueta que refresca")
+elif temperatura == 15:
+    print("Fa un temps de ni fu ni fa")
+else:
+    print("Quin temps mes bo que fa")
