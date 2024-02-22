4f1db098d1aa317ac8e6ca1c821773d65a025796
BielGonzalez <158280733+BielGonzalez@users.noreply.github.com>
2024-02-23 00:27:55 +0100

    Create bateria4.2.py

---
+++ bateria4.2.py
@@ -0,0 +1,21 @@
+contador = 0
+a = 0
+while True:
+    num = int(input("Digues un numero. "))
+    contador +=1
+    if num == 0:
+        break
+    else:
+        a += num
+a = a / contador
+print(a)contador = 0
+a = 0
+while True:
+    num = int(input("Digues un numero. "))
+    contador +=1
+    if num == 0:
+        break
+    else:
+        a += num
+a = a / contador
+print(a)
