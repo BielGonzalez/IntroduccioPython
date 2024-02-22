c199d73741069e6127a430f5ffb563b65f2fa350
BielGonzalez <158280733+BielGonzalez@users.noreply.github.com>
2024-02-23 00:11:49 +0100

    Create bateria3.5.py

---
+++ bateria3.5.py
@@ -0,0 +1,11 @@
+numero = 1
+while numero <= 10:
+    contador = 1
+    a = 0
+    while contador <= numero:
+        if numero % contador == 0:
+            a+=1
+        contador = contador + 1
+    if a == 2:
+        print(numero)
+    numero +=1
