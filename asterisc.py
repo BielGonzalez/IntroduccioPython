907b93398a691bc085d9dc5df3ae036ac1dabedb
BielGonzalez <158280733+BielGonzalez@users.noreply.github.com>
2024-02-29 09:36:46 +0100

    Create asterisc.py

---
+++ asterisc.py
@@ -0,0 +1,41 @@
+moviments = [" ", " ", " ", " ",  " ", " ", " ", " ", " ", "*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+vides = int(input("Quantes vides tens?"))
+while True:
+    mov = input("")
+    if mov == "a":
+        posicion_asterisc = moviments.index("*")
+        if posicion_asterisc == 0:
+            # funcion map convierte cada elemento de la lista con la funcion indicada en este caso str.
+            lista_convertida = map(str, moviments)
+            # Join une todos los strings creados por map anteriormente en uno solo
+            resultado = " ".join(lista_convertida)
+            print(resultado)
+        else:
+            moviments.pop(posicion_asterisc)
+            moviments.insert(posicion_asterisc - 1, "*")
+            # funcion map convierte cada elemento de la lista con la funcion indicada en este caso str.
+            lista_convertida = map(str, moviments)
+            # Join une todos los strings creados por map anteriormente en uno solo
+            resultado = " ".join(lista_convertida)
+            print(resultado)
+    if mov == "d":
+        posicion_asterisc2 = moviments.index("*")
+        if posicion_asterisc2 == 19:
+            # funcion map convierte cada elemento de la lista con la funcion indicada en este caso str.
+            lista_convertida = map(str, moviments)
+            # Join une todos los strings creados por map anteriormente en uno solo
+            resultado = " ".join(lista_convertida)
+            print(resultado)
+        else:
+            moviments.pop(posicion_asterisc2)
+            moviments.insert(posicion_asterisc2 + 1,"*")
+            # funcion map convierte cada elemento de la lista con la funcion indicada en este caso str.
+            lista_convertida = map(str, moviments)
+            # Join une todos los strings creados por map anteriormente en uno solo
+            resultado = " ".join(lista_convertida)
+            print(resultado)
+    if mov == "k":
+        vides -= 1
+    if vides == 0:
+        print("Game Over")
+        break
